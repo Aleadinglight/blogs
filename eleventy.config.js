a1e2359d3e1750facb57850ff31f5384abda2080
Aleadinglight <tam.nguyen@mapbox.com>
2025-03-30 04:35:03 +0300

    Add layouts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,9 @@
 export default async function(eleventyConfig) {
 	// Configure Eleventy
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+  eleventyConfig.setLayoutResolution(false);
+
   return {
     dir: {
       input: "src",      // your input directory (adjust if different)
